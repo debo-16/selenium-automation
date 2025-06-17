b4646818c17ae677c775edba8be5324bc1e30bb6
Deboleena chowdhury <deboleenachowdhury16@gmail.com>
2025-06-17 19:49:51 +0530

    Update brokenImages.js

--- test/brokenImages.js
+++ test/brokenImages.js
@@ -1,49 +1,20 @@
 import { Builder, Browser, By, until } from 'selenium-webdriver';
-//import assert from "assert";
 
 
 
-describe("check images are broken or not", function(){
-  it("it succesfully checks the images",async function () {
-    // launch browser
-  let driver = new Builder().forBrowser(Browser.CHROME).build();
-  await driver.manage().window().maximize();
-  //navigate to app
-  await driver.get('https://the-internet.herokuapp.com/broken_images');
 
-  //try {
-    
-    
-     
-    await driver.wait(until.elementsLocated(By.css('img')), 5000);
-    // Check in browser if it's loaded properly
-    const container = await driver.findElement(By.id('content'));
 
-    // ✅ Get all img elements inside the container
-    const images = await container.findElements(By.css('img'));
+(async function AbortController() {
+  const args = process.argv.slice(2);
+let browser = 'chrome';
 
-    for (let i=0; i<`${images.length}`; i++){
-        const isBroken = await driver.executeScript(`
-          const img = arguments[0];
-          return !(img && img.complete && img.naturalWidth > 0);
-        `, images[i]);
-  
-        console.log("isBroken",isBroken);
-        
-    }
-
-   
-  //}
-  //catch(err) {
-    //console.log("error is: ",err);
-  //}
-  });
+args.forEach(arg => {
+  if (arg.startsWith('--browser=')) {
+    browser = arg.split('=')[1];
+  }
 });
 
-
-
-
-/*(async function AbortController() {
+console.log(`Running tests in ${browser}...`);
   // launch browser
   let driver = new Builder().forBrowser(Browser.CHROME).build();
   await driver.manage().window().maximize();
@@ -83,6 +54,6 @@ describe("check images are broken or not", function(){
  
 
 
-}) ();*/
+}) ();
 
 
